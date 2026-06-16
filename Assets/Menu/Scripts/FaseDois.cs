7df7aee05bc433ac9900cf87c562fe17290eb73d
Daniel <mocaibre@gmail.com>
2026-06-16 20:59:14 -0300

    conectando as fases

---
+++ Assets/Menu/Scripts/FaseDois.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FaseDois : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene("Fase 2");
+    }
+}
